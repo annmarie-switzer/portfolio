ca5941c71d2e1000de3efd34493dabf2593ea5de
Annmarie Argiros <aargiros@gmail.com>
2017-10-08 14:51:29 -0400

    fix settings

--- portfolio/settings/base_settings.py
+++ portfolio/settings/base_settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os, dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
