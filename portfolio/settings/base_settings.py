f1c97b457b3d9c2f581fa0b730b97c068f839605
Annmarie Argiros <aargiros@gmail.com>
2017-10-08 15:14:34 -0400

    edited allowed hosts

--- portfolio/settings/base_settings.py
+++ portfolio/settings/base_settings.py
@@ -120,7 +120,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['annmarie.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
