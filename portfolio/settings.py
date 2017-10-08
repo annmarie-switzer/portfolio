2ecfe4eb02c8cd885e1f1dc42d442adb1b81e96d
Annmarie Argiros <aargiros@gmail.com>
2017-10-08 13:23:27 -0400

    still messing with environ vars and heroku config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-# from .settings_secret import sk
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "os.environ['SECRET_KEY']"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
