7339d8e566013d782112c7f9e807ae3ac00164e0
Annmarie Argiros <aargiros@gmail.com>
2017-10-11 23:25:48 -0400

    cleaned up the copy and a bit of formatting

--- wikipedia/static/wikipedia/js/wikipedia.js
+++ wikipedia/static/wikipedia/js/wikipedia.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 		e.preventDefault();
 		$("#results").html("");
 		var search_term = $("#search-term").val();
+		
 		$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search_term + "&format=json&callback=?", function(json) {
 
 			for (i=0; i<json[1].length; i++) {
@@ -11,13 +12,12 @@ $(document).ready(function() {
 				var text = json[2][i];
 				var link = json[3][i];
 
-				var title_element = $("<h4></h4>");
-				var link_element = $("<a></a>").attr("href", link).attr("target", "_blank").text(title);
+				var link_element = $("<a></a>").attr("href", link).attr("target", "_blank");
+				var title_element = $("<h4></h4>").text(title);
 				var text_element = $("<p></p>").text(text);
 
-				$(title_element).wrapInner(link_element);
-				$("#results").append(title_element);
-				$("#results").append(text_element);
+				$(link_element).append(title_element, text_element)
+				$("#results").append(link_element).append("<hr>");
 
 			};
 			if ( $("#results").html() == "" ) {
