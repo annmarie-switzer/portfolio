5ce419fa469d71b797c458850a6165c8af0d30e0
Annmarie Argiros <aargiros@gmail.com>
2017-10-16 22:51:19 -0400

    about me additions

--- about/views.py
+++ about/views.py
@@ -5,23 +5,22 @@ from django.shortcuts import render, get_object_or_404
 def get(request):
 
 	about_data = {
-		'Preferred mobile device': 'Currently I have an iPhone but as soon as I can justify the cost, I\'m getting a Pixel.',
-
+	'Education': 'I have a B.A. in English and nearly two years of independent study in Programming and Web Devleopment. See my <a href="https://docs.google.com/document/d/103V8QM5Jolj0HsdzSuwHZCPD1LXalgJyyz-AeARKHuo/edit?usp=sharing" class="colored-link font-weight-bold">resume</a> for more details.',
 		'Generation': '"She\'s an older millenial, sir, but she checks out."',
-
 		'Feelings about Star Wars': 'I think I already answered this.',
-
-		'Married': 'Yes.',
-
-		'Children': 'No.',
-
+		'Preferred mobile device': 'Current: iPhone<br>Future: Pixel',
+		'Tattoos': 'Yes',
+		'Married': 'Yes',
+		'Children': 'No',
 		'Completed a marathon': 'Many times! My sister would make cookies while I got everything organized. I had to make sure each disc was in the DVD player in the right order. There is nothing more immersion-shattering than a fade-out in Rivendell and a fade-in at Helm\'s Deep!',
-
-		'Tattoos': 'Yes.',
-
+		'Music to work to': 'Tycho',
+		'Music to work out to': 'Blind Guardian',
+		'Coffee': 'Black',
 		'Method of note-taking': 'Digital, and even that\'s a stuggle sometimes. My brain moves too fast for my hands :P.',
-
+		'Why programming': 'We have to stay one step ahead of Skynet.',
+		'Seriously, though': 'I get to use my intelligence, creativity, and capacity and desire to learn, to contribute to society in a way I was never able to do before learning to code.',
 		'Years of service': 'That\'s your call.',
+		
 	}
 
 	context = {
