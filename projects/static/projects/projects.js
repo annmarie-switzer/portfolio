c10801aa6e9702888039c61866780c93eb485936
unknown <aargiros@gmail.com>
2019-09-19 23:57:54 -0400

    completed new project page

--- projects/static/projects/projects.js
+++ projects/static/projects/projects.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
+	$("#btn-detect").addClass("active-proj");
+	$(".scroll-button").on("click", function () {
+		let els = document.getElementsByClassName("active-proj");
+		while(els.length) {
+			els[0].classList.remove("active-proj")
+		}
+		$(this).addClass("active-proj");
+	});
 
-
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip()
+	})
 
 }); //End Document
--- projects/views.py
+++ projects/views.py
@@ -57,17 +57,59 @@ def projects(request):
 	
 	projects = [
 		{
+			'id': 'detect',
+			'info':
+			'''
+			<span class="icon-angular mr-3" data-toggle="tooltip" data-placement="bottom" title="Angular"></span>
+			<span class="icon-typescript mr-3" data-toggle="tooltip" data-placement="bottom" title="TypeScript"></span>
+			<span class="icon-javascript mr-3" data-toggle="tooltip" data-placement="bottom" title="JavaScript"></span>
+			<span class="icon-materialdesign mr-3" data-toggle="tooltip" data-placement="bottom" title="Material"></span>
+			<span class="icon-docker mr-3" data-toggle="tooltip" data-placement="bottom" title="Docker"></span>
+			''',
 			'name': 'DataDetect',
-			'description': 'An application for file system data analysis and action. The frontend is an Angular 7 web app connecting to a .NET/Elastic/PostgreSQL backend, styled with the Angular Material component library. <br><br> The UI was designed with the idea that a user will want to "scan, act, and move on." Therefore, the workflow is very concise but also extremely versatile, giving a user multiple ways to search, analyze, and tag documents, as well as maintaing query terms through navigation events.'
+			'description': 'An application for file system data analysis and action. The frontend is an Angular 7 web app connecting to a .NET/Elastic/PostgreSQL backend, styled with the Angular Material component library. <br><br> The UI was designed with the idea that a user will want to "scan, act, and move on." Therefore, the workflow is very concise but also extremely versatile, giving a user multiple ways to search, analyze, and tag documents, as well as maintaing query terms through navigation events.',
+			'video': 'Not publically available',
 		},
 		{
+			'id': 'connect',
+			'info':
+			'''
+			<span class="icon-angular mr-3" data-toggle="tooltip" data-placement="bottom" title="Angular"></span>
+			<span class="icon-typescript mr-3" data-toggle="tooltip" data-placement="bottom" title="TypeScript"></span>
+			<span class="icon-javascript mr-3" data-toggle="tooltip" data-placement="bottom" title="JavaScript"></span>
+			<span class="icon-materialdesign mr-3" data-toggle="tooltip" data-placement="bottom" title="Material"></span>
+			''',
 			'name': 'Connector Framework',
-			'description': 'A tool used to get data from social media platforms and transform that data into an archiveable format. The frontend is Angular 8, the backend is .NET/MariaDB, and styling is based on the Angular Material component library. <br><br>  The core functionality of this application is based on a very long and potentially confusing list of config options, so the UI utilizes a wizard-style workflow with carefully styled calls-to-action to make the user workflow as intuitive and simple as possible.'
+			'description': 'A tool used to get data from social media platforms and transform that data into an archiveable format. The frontend is Angular 8, the backend is .NET/MariaDB, and styling is based on the Angular Material component library. <br><br>  The core functionality of this application is based on a very long and potentially confusing list of config options, so the UI utilizes a wizard-style workflow with carefully styled calls-to-action to make the user workflow as intuitive and simple as possible.',
+			'video': 'Not publically available',
 		},
 		{
+			'id': 'spark',
+			'info': 
+			'''
+			<span class="icon-python mr-3" data-toggle="tooltip" data-placement="bottom" title="Python"></span>
+			<span class="icon-django mr-3" data-toggle="tooltip" data-placement="bottom" title="Django"></span>
+			<span class="icon-javascript mr-3" data-toggle="tooltip" data-placement="bottom" title="JavaScript"></span>
+			<span class="icon-jquery mr-3" data-toggle="tooltip" data-placement="bottom" title="jQuery"></span>
+			<span class="icon-bootstrap mr-3" data-toggle="tooltip" data-placement="bottom" title="Bootstrap"></span>
+			<span class="icon-postgresql mr-3" data-toggle="tooltip" data-placement="bottom" title="PostgreSQL"></span>
+			''',
 			'name': 'Sparkit',
-			'description': 'Sparkit is an application that connects event hosts to prospective event goers. My primary responsibility for the application has been user CRUD functionality and authentication, as well as styling the associated pages. The application uses Django\'s model-based forms and authentication middleware along with PostgreSQL.'
+			'description': 'Sparkit is an application that connects event hosts to prospective event goers. My primary responsibility for the application has been user CRUD functionality and authentication, as well as styling the associated pages. The application uses Django\'s model-based forms and authentication middleware along with PostgreSQL.',
+			'video': '<iframe width="100%" height="315" src="https://www.youtube.com/embed/TsBr2hWVnnA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 		},
+		{
+			'id': 'fcc',
+			'info': 
+			'''
+			<span class="icon-javascript mr-3" data-toggle="tooltip" data-placement="bottom" title="JavaScript"></span>
+			<span class="icon-jquery mr-3" data-toggle="tooltip" data-placement="bottom" title="jQuery"></span>
+			<span class="icon-bootstrap mr-3" data-toggle="tooltip" data-placement="bottom" title="Bootstrap"></span>
+			''',
+			'name': 'Other',
+			'description': '',
+			'video': 'A collection of projects for <a href="https://www.freecodecamp.org/bellamira">freeCodeCamp\'s Frontend and JavaScript certifications.</a>',
+		}
 	]
 
 	context = {
